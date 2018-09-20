70969b709b872595f2233491ec90072d995dfe04
shifrank1989 <shifrank1989@frankdemacbook-pro.local>
2018-09-20 11:25:46 +0800

    supermicro终版

--- CDS2000/CDS2000/main.c
+++ CDS2000/CDS2000/main.c
@@ -12,6 +12,12 @@
 #include <string.h>
 #include "cJSON.h"
 #define MAXBUFSIZE 1024
+typedef struct ClusterInfo{
+    char physicalCapacity[50];
+    char PhysicalUsage[50];
+    //int  nodeCount;
+}CLUSTER ;
+
 typedef  struct occupy
 {
     char name[20];
@@ -37,19 +43,22 @@ void get_mem_occupy(MEM_OCCUPY * mem);
 double get_memory_total();
 double get_memory_free();
 char * POST_Authorization();
-char * get_Cluster_Status();
+//char * get_Cluster_Status();
+CLUSTER * get_Cluster_Status();
 char * Splice_character();
 //char *character;
 char * get_accessToken();
 float  g_cpu_used;
 char  * cpu_temperature;
 char * Authorization;
-char authorization_cmd[500]="curl -s -k -X GET 'https://192.168.0.12/irisservices/api/v1/public/cluster?fetchStats=true' -H 'accept: application/json' -H 'Authorization:Bearer ";
+char * get_Fan_Status();
+char fanstatus[5];
+char authorization_cmd[1024]="curl -s -k -X GET 'https://192.168.0.13/irisservices/api/v1/public/cluster?fetchStats=true' -H 'accept: application/json' -H 'Authorization:Bearer ";
 
 
 int main(int argc, const char * argv[]) {
     printf("-- It time to test -- \n");
-    printf("cpu num: %i\n",get_cpu_num());
+    //printf("cpu num: %i\n",get_cpu_num());
     //printf("cpu used: %4.2f \n",get_cpu_occupy());
     //char a[20]="'CPU1 Temp'";
     //char *cpu_id=a;
@@ -59,8 +68,14 @@ int main(int argc, const char * argv[]) {
     //printf("memory free :%4.2f\n",get_memory_free());
     //printf("authorization info : %s\n",POST_Authorization());
     
-    printf("physicalCapacityBytes :%s\n",get_Cluster_Status());
+    //printf("physicalCapacityBytes :%s\n",get_Cluster_Status()->physicalCapacity);
+    //printf("PhysicalUsage :%s\n",get_Cluster_Status()->PhysicalUsage);
+    //free(get_Cluster_Status());
+    CLUSTER *pclu = get_Cluster_Status();
+    printf("physicalCapacityBytes :%s\n",pclu->physicalCapacity);
+    printf("PhysicalUsage :%s\n",pclu->PhysicalUsage);
     //Splice_character();
+    //printf("fanstatus %s \n",get_Fan_Status());
     
     return 0;
 }
@@ -167,13 +182,18 @@ int get_net_num(){
     return net_num;
 }
 //集群总容量
-char * get_Cluster_Status(){
+CLUSTER * get_Cluster_Status(){
     FILE * cluster_status;
+    CLUSTER *pCluinfo = malloc(sizeof(CLUSTER));
+    //CLUSTER *buff = malloc(sizeof(buff));
     char buff[2048];
     char a[20];
     //char b[20];
     char c[50];
-    Splice_character();
+    char d[50];
+    //int e;
+    //Splice_character();
+    POST_Authorization();
     char * cmd ;
     cmd=authorization_cmd;
     cluster_status = popen(cmd,"r");
@@ -182,9 +202,11 @@ char * get_Cluster_Status(){
     if(fgets (buff, sizeof(buff), cluster_status)!=NULL){
         //printf("标记：%s\n","mmmmm");
         sscanf (buff, "%s\n",a);
-        //printf("cluster info: %s\n",a);
+        printf("cluster info: %s\n",a);
         cJSON * root = NULL;
         cJSON * item = NULL;//cjson对象
+        cJSON * physicalCapacity=NULL;
+        cJSON * totalPhysicalUsageBytes=NULL;
         root = cJSON_Parse(a);
         if (!root)
         {
@@ -194,32 +216,45 @@ char * get_Cluster_Status(){
         {
             //printf("标记：%s\n","jjjjj");
             item = cJSON_GetObjectItem(root, "stats");
-            //printf("1: %s\n", cJSON_Print(item));
+            printf("1: %s\n", cJSON_Print(item));
             item = cJSON_GetObjectItem(item, "localUsagePerfStats");
             //printf("2: %s\n", cJSON_Print(item));
-            item = cJSON_GetObjectItem(item, "physicalCapacityBytes");
-            sprintf(c,"%lf",item->valuedouble);
+            physicalCapacity = cJSON_GetObjectItem(item, "physicalCapacityBytes");
+            sprintf(c,"%4.2f",((physicalCapacity->valuedouble)/1024/1024/1024/1024));
+            //sprintf(e,"%d",c);
+            strcpy(pCluinfo->physicalCapacity, c) ;
+            totalPhysicalUsageBytes=cJSON_GetObjectItem(item, "totalPhysicalUsageBytes");
+            sprintf(d,"%4.1f",totalPhysicalUsageBytes->valuedouble);
+            strcpy(pCluinfo->PhysicalUsage,d) ;
             //printf("3: %s \n",c);
             //sprintf(c, "%g", item->valuedouble);
             //strcpy(*clusterstatus,c);
             //printf("3: %f \n",item->valuedouble);
+            //free(buff);
             cJSON_Delete(root);
+            //cJSON_Delete(item);
         }
     }
     pclose(cluster_status);
-    return c;
+    free(pCluinfo);
+    //free(buff);
+    return pCluinfo;
 }
+
 char * POST_Authorization(){
     //char * Authorization=NULL;
     char buff[MAXBUFSIZE];
     char a[20];
     char * accessToken;
-    FILE * authorization=NULL;
+    FILE * authorization;
     
-    char * cmd= "curl -s -k -X POST 'https://192.168.0.12/irisservices/api/v1/public/accessTokens' -H 'accept: application/json' -H 'content-type: application/json' -d '{ \"domain\": \"LOCAL\", \"password\": \"admin\", \"username\": \"admin\"}'";
+    char * cmd= "curl -s -k -X POST 'https://192.168.0.13/irisservices/api/v1/public/accessTokens' -H 'accept: application/json' -H 'content-type: application/json' -d '{ \"domain\": \"LOCAL\", \"password\": \"admin\", \"username\": \"admin\"}'";
     authorization = popen(cmd, "r");
+    //cJSON * root=cJSON_CreateObject();
+    //cJSON * item = NULL;
     if(fgets (buff, sizeof(buff), authorization)!=NULL){
         sscanf (buff, "%s\n",a);
+        //free(buff);
         cJSON * root = NULL;
         cJSON * item = NULL;
         //printf("quan %s\n",a);
@@ -232,22 +267,61 @@ char * POST_Authorization(){
         else
         {
             item = cJSON_GetObjectItem(root, "accessToken");
+            //printf("1: %s\n", cJSON_Print(item));
             accessToken=item->valuestring;
+            //strcpy(accessToken,item->valuestring);
+            //printf("authorization info: %s\n",accessToken);
         }
+        //if (root)
+        // cJSON_Delete(root);
         //cJSON_Delete(root);
         //cJSON_Delete(item);
-        //printf("authorization info: %s\n",accessToken);
+       // printf("authorization info: %s\n",accessToken);
     }
     //Authorization=a;
-    
+    //cJSON_Delete(root);
     pclose(authorization);
-    
-    return accessToken;
-}
-char * Splice_character(){
-    char * character;
-    strcat (authorization_cmd,POST_Authorization());
+    //    if (root)
+    //        cJSON_Delete(root);
+    //free(buff);
+    strcat (authorization_cmd,accessToken);
     strcat (authorization_cmd,"'");
-    character=authorization_cmd;
     return authorization_cmd;
+    //return accessToken;
+}
+//char * Splice_character(){
+//    //char * character;
+//    strcat (authorization_cmd,POST_Authorization());
+//   // printf(POST_Authorization());
+//    //free(POST_Authorization());
+//    strcat (authorization_cmd,"'");
+//    //character=authorization_cmd;
+//    return authorization_cmd;
+//}
+//风扇状态
+char * get_Fan_Status(){
+    //char * fanstatus;
+    FILE * fan_status;
+    char buff[MAXBUFSIZE];
+    char a[20];
+    char b[20];
+    char c[20];
+    //char d[20];
+    //fan_status = popen("sudo ipmitool sensor get 'Sys Fan 1A'","r");
+    fan_status = popen("sudo ipmitool sensor get 'Sys Fan 1A'","r");
+    
+    int i=0;
+    
+    while(i++<30){
+        if(fgets (buff, sizeof(buff), fan_status)!=NULL){
+            sscanf (buff, "%s %s %s ",a,b,c);
+            if(strcmp(a, "Status")==0){
+                printf("fan status :%s \n",c);
+                //fanstatus=c;
+                strcpy(fanstatus,c);
+            }
+        }
+    }
+    fclose(fan_status);
+    return fanstatus;
 }
---
+++ CDS2000/cds2000.h
@@ -0,0 +1,14 @@
+//
+//  cds2000.h
+//  CDS2000
+//
+//  Created by shifrank1989 on 2018/9/12.
+//  Copyright © 2018年 shifrank1989. All rights reserved.
+//
+
+#ifndef cds2000_h
+#define cds2000_h
+
+#include <stdio.h>
+
+#endif /* cds2000_h */
